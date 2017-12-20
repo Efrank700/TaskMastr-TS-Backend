7e66c429395b517031f3041132d7053261c38654
Unknown <ericjosephfrank@gmail.com>
2017-12-19 22:34:10 -0500

    method update for mongo driver
    Added async methods to the mongo driver. TODO: add material quantity removal and bcryptJS for password protection. Package lock also added after NPM update

--- src/DBDriver/DBDriver.ts
+++ src/DBDriver/DBDriver.ts
@@ -1,8 +1,9 @@
 import * as mongoose from "mongoose";
 import {eventStore} from './EventStore';
 import {keyStore} from './KeyStore';
-import {participantTypes} from '../Participant';
-import {TaskMastrEvent} from '../Event'
+import {participantTypes, admin, supervisor, runner, participant} from '../Participant';
+import {TaskMastrEvent} from '../Event';
+import * as bcrypt from "bcryptjs";
 
 mongoose.connect('mongodb://127.0.0.1:27017');
 
@@ -20,7 +21,8 @@ class MongoDriver {
             toStore.save();
             return num;
         } catch (error) {
-            throw new Error(error);
+            const castError = error as Error
+            throw new Error(castError.message);
         }
     }
 
@@ -29,24 +31,197 @@ class MongoDriver {
             let res = [await this.generateKey(), await this.generateKey(), await this.generateKey()];
             return res;
         } catch (error) {
-            throw new Error(error);
+            const castError = error as Error
+            throw new Error(castError.message);
         }
     }
 
+    public static async userNameAvailable(evKey: number, userName: string): 
+                                          Promise<Boolean | null> {
+        try {
+            let res = await eventStore.findOne().or([
+                {adminKey: evKey}, 
+                {supervisorKey: evKey}, 
+                {runnerKey: evKey}]);
+            if(res === null) return null;
+            return(res.logins.findIndex((target) => {return target.user === userName}) === -1);
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
 
+    public static async screenNameAvailable(evKey: number, screenName: string): 
+                                            Promise<Boolean | null> {
+        try {
+            let res = await eventStore.findOne().or([
+                {adminKey: evKey}, 
+                {supervisorKey: evKey}, 
+                {runnerKey: evKey}]);
+            if(res === null) return null;
+            return(res.logins.findIndex((target) => {return target.screenName === screenName}) === -1);
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async addUser(evKey: number, userName: string, userScreen: string, 
+                                userPass: string): Promise<participantTypes | null> {
+        try {
+            let saltPromise = bcrypt.genSalt(10)
+            let res = await eventStore.findOne().or([
+                {adminKey: evKey}, 
+                {supervisorKey: evKey}, 
+                {runnerKey: evKey}]);
+            if(res === null) return null;
+            let passPromise = bcrypt.hash(userPass, await saltPromise);
+            let index = res.logins.findIndex((user) => {return user.user === userName 
+                                                        || user.screenName === userScreen});
+            if(index > -1) throw new Error('SUEXISTS');
+            let position;
+            if(evKey === res.adminKey) position = 0;
+            else if(evKey === res.supervisorKey) position = 1;
+            else position = 2;
+            eventStore.findOneAndUpdate({eventName: res.eventName}, 
+                {$push: {logins: {user: userName, pass: await passPromise, screenName: userScreen,
+                                  pos: position}}});
+            let retVal: participantTypes;
+            if(position === 0) retVal = participantTypes.admin;
+            else if(position === 1) retVal = participantTypes.supervisor;
+            else retVal = participantTypes.runner;
+            return retVal;
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async athenticate(evKey: number, userName: string, userPass: string): 
+                                    Promise<[boolean, string, participantTypes]> {
+        try {
+            let res = await eventStore.findOne().or([
+                {adminKey: evKey}, 
+                {supervisorKey: evKey}, 
+                {runnerKey: evKey}]);
+            if(res === null) return [false, "NSEVENT", participantTypes.admin];
+            let targetIndex = res.logins.findIndex((user) => {return user.user === userName});
+            if(targetIndex === -1) return [false, "NSUSER", participantTypes.admin];
+            const targetUser = res.logins[targetIndex];
+            let passMatchPromise = bcrypt.compare(userPass, targetUser.pass);
+            let participantType: participantTypes;
+            if(targetUser.pos === 0) participantType = participantTypes.admin;
+            else if(targetUser.pos === 1) participantType = participantTypes.supervisor;
+            else participantType = participantTypes.runner;
+            if(!(await passMatchPromise)) return([false, "IPASS", participantTypes.admin]);
+            return([true, targetUser.screenName, participantType]);
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async deleteUser(evKey: number, userName: string): Promise<Boolean | null> {
+        try {
+            let res = await eventStore.findOneAndUpdate({$or: [{adminKey: evKey}, 
+                {supervisorKey: evKey}, {runnerKey: evKey}]}, 
+                {$pull: {logins: {user: userName}}});
+            if(res === null) return null;
+            else return(res.logins.findIndex((user) => {return user.user === userName}) !== -1)
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async createEvent(eventName: string, owner: admin, ownerUser: string, 
+                                    ownerPass: string): Promise<TaskMastrEvent | null> {
+        try {
+            let keysPromise = this.generateKeySet();
+            let nameExists = (await eventStore.findOne({'eventName': eventName})) === null;
+            if(nameExists) return null;
+            let keys = await keysPromise;
+            let evToSave = new eventStore({
+                eventName: eventName, 
+                adminKey: keys[0],
+                supervisorKey: keys[1],
+                runnerKey: keys[2],
+                owner: {user: ownerUser, pass: ownerPass, screenName: owner.screenName, pos: 0},
+                logins: [{user: ownerUser, pass: ownerPass, screenName: owner.screenName, pos: 0}],
+                materials: []
+            })
+            evToSave.save();
+            owner.roomName = eventName;
+            return new TaskMastrEvent(eventName, keys[0], keys[1], keys[2], owner, []);
+        }
+        catch (error){
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
 
     public static async retrieveEvent(evKey: number): Promise<TaskMastrEvent | null> {
         try {
-            let res = await eventStore.findOne().or([{adminKey: evKey}, {supervisorKey: evKey}, {runnerKey: evKey}]);
-                if(res === null) return null;
-                const evName = res.eventName;
-                const keys = [res.adminKey, res.supervisorKey, res.runnerKey];
-                const materials = res.materials;
-                const owner = res.owner;
-                let newEV = new TaskMastrEvent(evName, keys[0], keys[1], keys[2], owner, materials);
-                return newEV;
+            let res = await eventStore.findOne().or([{adminKey: evKey}, 
+                                                     {supervisorKey: evKey}, 
+                                                     {runnerKey: evKey}]);
+            if(res === null) return null;
+            const evName = res.eventName;
+            const keys = [res.adminKey, res.supervisorKey, res.runnerKey];
+            const materials = res.materials;
+            const owner = res.owner;
+            return new TaskMastrEvent(evName, keys[0], keys[1], keys[2], owner, materials);
         } catch (error) {
-            throw new Error
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async deleteEvent(eventIdentifier: number | string): Promise<Boolean | null> {
+        try {
+            if(typeof eventIdentifier === "string") {
+                let res = await eventStore.findOne({adminKey: eventIdentifier});
+                if(res === null) return null;
+                let delResponse = await res.remove();
+                return (delResponse != null);
+            }
+            else {
+                let res = await eventStore.findOne({eventName: eventIdentifier});
+                if(res === null) return null;
+                let delResponse = await res.remove();
+                return (delResponse != null);
+            }
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
+        }
+    }
+
+    public static async addMaterials(evKey: number, materialName: string, quantity: number): 
+                                     Promise<boolean | null> {
+        try {
+            let resPromise = eventStore.findOne().or([{adminKey: evKey}, 
+                                                     {supervisorKey: evKey}]);
+            if(quantity <= 0) return null;
+            let res = await resPromise;
+            if(res === null) return null;
+            let materials = res.materials;
+            const targetIndex = materials.findIndex((target) => {
+                return target.itemName === materialName
+            })
+            if(targetIndex !== -1) {
+                res.materials[targetIndex].count += quantity;
+                await res.save();
+                return true;
+            }
+            else {
+                res.materials.push({itemName: materialName, count: quantity});
+                await res.save();
+                return false;
+            }
+        } catch (error) {
+            const castError = error as Error
+            throw new Error(castError.message);
         }
     }
 }
